2d1d1c296052ad8a6cc407e46a57f9be8892eb53
albert-611 <albertprogramming611@gmail.com>
2026-01-30 15:58:17 +0600

    DSA

---
+++ 09_Recursion/Recursion_Part_1/06_FirstOccurence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int firstOccur(vector<int> &arr, int i, int target){
+
+if (i == arr.size())
+{
+    return -1;
+}
+
+if (arr[i] == target)
+{
+    return i;
+}
+
+return firstOccur(arr, i+1, target);
+    
+}
+
+int main(){
+    
+    vector<int> arr = {1, 2, 3, 3, 3, 4};
+    int i = 0;
+    int target = 3;
+
+    cout << firstOccur(arr, i, target);
+    return 0;
+}
