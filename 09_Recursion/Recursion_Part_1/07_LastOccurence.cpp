cc50ed6470b3e132bd4ddb783f93effa9dba4808
albert-611 <albertprogramming611@gmail.com>
2026-01-30 16:16:22 +0600

    DSA

---
+++ 09_Recursion/Recursion_Part_1/07_LastOccurence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int LastOccur(vector<int> &arr, int i, int target){
+
+
+if (i < 0)
+{
+    return -1;
+}
+
+if (arr[i] == target)
+{
+    return i;
+}
+
+return LastOccur(arr, i-1, target);
+}
+
+int main(){
+    
+    vector<int> arr = {1, 2, 3, 3, 3, 4};
+    int i = arr.size() - 1;
+    int target = 3;
+
+    cout << LastOccur(arr, i, target);
+    return 0;
+}
