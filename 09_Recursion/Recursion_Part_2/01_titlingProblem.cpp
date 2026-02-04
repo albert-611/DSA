6ca5aaea38ec460e5fb6dcf8d95045b96679727c
albert-611 <albertprogramming611@gmail.com>
2026-02-04 21:12:31 +0600

    DSA

---
+++ 09_Recursion/Recursion_Part_2/01_titlingProblem.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int tilingProblem(int n){ // 2xn
+
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    
+    // // verticle
+    // int ans1 = ; // 2xn-1
+
+    // // horizontal
+    // int ans2 = tilingProblem(n - 2);
+
+    return tilingProblem(n - 1) + tilingProblem(n - 2);
+}
+int main(){
+
+    int n = 5;
+    cout<<tilingProblem(n)<< endl;
+    return 0;
+}
