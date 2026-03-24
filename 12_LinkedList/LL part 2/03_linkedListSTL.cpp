64a50db60657f39ff01a2215ee67ff8d9a4f189a
albert-611 <albertprogramming611@gmail.com>
2026-03-24 22:37:40 +0600

    DSA

--- 12_LinkedList/LL part 2/03_linkedListSTL.cpp
+++ 12_LinkedList/LL part 2/03_linkedListSTL.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void printList(const list<int>& ll){ //faster version
     for (auto itr = ll.begin(); itr != ll.end(); itr++){
-        cout <<(*itr) << " -> ";
+        cout <<*itr << " -> ";
     }
     cout <<"NULL"<<endl;
 }
@@ -23,7 +23,7 @@ int main()
 {
     list<int> ll;
 
-    ll.push_front(4);
+    ll.push_front(4); 
     ll.push_front(3);
     ll.push_front(2);
     ll.push_front(1);
@@ -47,11 +47,31 @@ int main()
 
     list<int> l2 = {7, 8, 9};
 
+    //! insert at any position
     auto it = ll.begin();
     advance(it, 2); // insert after 2nd position
 
     ll.insert(it, l2.begin(), l2.end());
 
+    printList(ll);
+
+    //! remove any value at any position
+    auto ite = ll.begin();
+    advance(ite, 2); // move to 3rd element
+    ll.erase(ite);   // removes that element
+    printList(ll);
+
+    //! remove any value in its range
+    auto start = ll.begin();
+    auto end = ll.begin();
+    advance(start, 1); // 2nd element
+    advance(end, 4);   // 5th element
+    ll.erase(start, end); // removes 2nd,3rd,4th elements
+
+    printList(ll);
+
+    //! removes all elements with value 5
+    ll.remove(5); 
     printList(ll);
     return 0;
 }
