2ee64be6b44ae5ed3b7eec9f302ee8508497c25b
albert-611 <albertprogramming611@gmail.com>
2026-05-10 23:48:11 +0600

    DSA

---
+++ 13_Stacks/Stack Part 1/Stack Implementation/01_4_2_stackimplementation_LinkedListwithClassTemplate.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+template< class T>
+
+class Node{
+    public:
+    T data;
+    Node* next;
+
+    Node(T val){
+        data = val;
+        next = NULL;
+    }
+};
+
+template< class T>
+class Stack{
+    Node<T>* head;
+
+    public:
+    Stack(){
+        head = NULL;
+    }
+
+    void push(T val){
+        Node<T>* newNode = new Node<T>(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            return;
+        }
+        newNode -> next = head;
+        head = newNode;
+        
+    }
+
+    void pop(){
+        Node<T>* temp = head;
+        head = head -> next;
+        temp -> next = NULL;
+        delete temp;
+    }
+
+    T top(){
+        return head -> data;
+    }
+
+    bool isEmpty(){
+        return head == NULL;
+    }
+};
+
+int main(){
+
+    Stack<int> s;
+
+    s.push(5);
+    s.push(4);
+    s.push(3);
+
+    while (!s.isEmpty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    return 0;
+}
